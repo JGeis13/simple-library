8487000705321edab5f5e7694e91c10c34f5f521
Jarick G <jarick13@gmail.com>
2021-05-17 15:42:04 -0600

    Completed basic logic. Added local storage functionality.

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
 (() => {
   let myLibrary = [];
-  let idCounter = 0;
 
   /* UI Elements */
 
   const formContainer = document.querySelector(".new-book-form");
   const formEl = document.querySelector(".new-book-form form");
+  const booksEl = document.querySelector(".books");
   const newBookBtn = document.querySelector("#new-book");
-  const saveNewBook = document.querySelector("#save-book");
-  const cancelNewBook = document.querySelector("#cancel-book");
-
-  /*  Dummy Data */
-  let tempData = [
-    { title: "Hello World!", author: "Billy The Kid", pageCount: 200, completed: false },
-    { title: "Goodbye again", author: "Stevie Jones", pageCount: 220, completed: true },
-  ];
-
-  // add tempData to library
-  for (let i = 0; i < tempData.length; i++) {
-    let { title, author, pageCount, completed } = tempData[i];
-    myLibrary.push(new Book(title, author, pageCount, completed));
-    idCounter++;
-  }
+  const saveNewBookBtn = document.querySelector("#save-book");
+  const cancelNewBookBtn = document.querySelector("#cancel-book");
 
   /* Logic */
 
-  function Book(title, author, pageCount, completed = false) {
+  function Book(id, title, author, pageCount, completed = false) {
     // constructor
-    this.id = idCounter;
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
@@ -38,33 +25,167 @@
     this.completed = !this.completed;
   };
 
+  function rawDataToLibrary(data) {
+    for (let i = 0; i < data.length; i++) {
+      let { id, title, author, pageCount, completed } = data[i];
+      myLibrary.push(new Book(id, title, author, pageCount, completed));
+    }
+  }
+
   function addNewBookToLibrary(title, author, pageCount, completed) {
-    myLibrary.push(new Book(title, author, pageCount, completed));
-    idCounter++;
+    myLibrary.push(new Book(idGenerator(), title, author, pageCount, completed));
+
+    updateLocalStorage(myLibrary);
     console.log(myLibrary);
   }
 
   function removeBookFromLibrary(id) {
     myLibrary = myLibrary.filter((book) => book.id != id);
+    updateLocalStorage(myLibrary);
     console.log(myLibrary);
   }
 
   function toggleCompletedBook(id) {
     myLibrary.filter((book) => book.id == id)[0].toggleCompleted();
+    updateLocalStorage(myLibrary);
+    console.log("Toggled complete on book id: ", id);
+    console.log(myLibrary);
+  }
+
+  function idGenerator() {
+    return Math.random().toString(36).substr(2, 8);
   }
 
   /* UI Functions */
 
   function displayLibrary() {
-    //
+    booksEl.innerHTML = "";
+    myLibrary.forEach((book) => {
+      booksEl.append(createBookElement(book));
+    });
   }
 
-  function createBookElement() {
+  function createBookElement(book) {
     //
+    const bookEl = document.createElement("div");
+    bookEl.classList.add("book");
+    bookEl.dataset.id = book.id;
+
+    ["title", "author", "pageCount", "completed", "delete"].forEach((prop) => {
+      const el = document.createElement("div");
+      el.classList.add(prop);
+      if (prop == "completed") {
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = book[prop];
+        el.append(checkbox);
+      } else if (prop == "delete") {
+        let delBtn = document.createElement("button");
+        delBtn.classList.add("delete-book");
+        delBtn.textContent = "x";
+        el.append(delBtn);
+      } else {
+        el.textContent = book[prop];
+      }
+
+      bookEl.append(el);
+    });
+
+    return bookEl;
+  }
+
+  function getFormData() {
+    return {
+      title: formEl.querySelector("[name='title']").value,
+      author: formEl.querySelector("[name='author']").value,
+      pageCount: formEl.querySelector("[name='page-count']").value,
+      completed: formEl.querySelector("[name='completed']").value == "yes",
+    };
+  }
+
+  function clearForm() {
+    formEl.querySelector("[name='title']").value = "";
+    formEl.querySelector("[name='author']").value = "";
+    formEl.querySelector("[name='page-count']").value = 200;
+    formEl.querySelector("[value='yes']").checked = false;
+    formEl.querySelector("[value='no']").checked = true;
   }
 
   /* Event Handlers */
 
+  booksEl.addEventListener("click", (e) => {
+    const targ = e.target;
+    const parent = targ.parentElement;
+
+    if (parent.classList.contains("completed")) {
+      // clicked a checkbox
+      // get data-id from parent book
+      const id = parent.parentElement.dataset.id;
+      toggleCompletedBook(id);
+    } else if (parent.classList.contains("delete")) {
+      // clicked a delete button
+      const id = parent.parentElement.dataset.id;
+      const book = myLibrary.filter((book) => book.id == id)[0];
+      const confirmed = confirm(`Are you sure you want to delete "${book.title}" ?`);
+
+      if (!confirmed) return;
+
+      removeBookFromLibrary(id);
+      // refresh display
+      displayLibrary();
+    }
+  });
+
+  newBookBtn.addEventListener("click", () => {
+    // show form
+    formContainer.style.display = "block";
+  });
+
+  cancelNewBookBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.style.display = "none";
+    clearForm();
+  });
+
+  formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addNewBookToLibrary(...Object.values(getFormData()));
+    displayLibrary();
+    clearForm();
+    formContainer.style.display = "none";
+  });
+
+  formContainer.addEventListener("click", (e) => {
+    // hide the form but don't clear if clicked outside form element
+    if (e.target.classList.contains("new-book-form")) {
+      formContainer.style.display = "none";
+    }
+  });
+
   /* LOCAL STORAGE */
+
+  function clearLocalStorage() {
+    localStorage.clear();
+  }
+
+  function updateLocalStorage(library) {
+    localStorage.setItem("simpleLibrary", JSON.stringify(library));
+  }
+
+  if (localStorage.getItem("simpleLibrary")) {
+    let bookData = JSON.parse(localStorage.getItem("simpleLibrary"));
+    rawDataToLibrary(bookData);
+    displayLibrary();
+  }
+
   /* FIREBASE STORAGE */
 })();
+
+/* 
+To-Do's
+-------
+- Styling
+- Animations
+- local storage
+
+*/
